e8694e0b36974fa92c5558e9cd64cf6ecc9515c8
neha pradhan <neha.pradhan@fusemachines.com>
2017-07-04 17:28:46 +0545

    babel-loaderadded to webpack config

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+class App extends Component {
+  render(){
+    return(
+<div>
+hello neha
+</div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))

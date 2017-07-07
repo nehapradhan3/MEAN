de1c45994f193e68656a17e90d67545c6883a688
neha pradhan <neha.pradhan@fusemachines.com>
2017-07-07 14:47:39 +0545

    stylesjs new file for styles

--- src/components/Zone.js
+++ src/components/Zone.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import styles from './styles'
 
 class Zone extends Component {
 
@@ -8,8 +9,8 @@ class Zone extends Component {
 
 
             <div style={styles.container}>
-              <h2 style={{marginBottom:0}}>
-              <a style={{textDecoration:'none', color:'red'}} href="#">{this.props.currentZone.name}</a>
+              <h2 style={styles.header}>
+              <a style={styles.title} href="#">{this.props.currentZone.name}</a>
               </h2>
               <span>{this.props.currentZone.aCode}</span><br />
               <span>{this.props.currentZone.numComments} Comments</span>
@@ -19,12 +20,5 @@ class Zone extends Component {
   }
 }
 
-const styles= {
-  container: {
-    padding:16,
-    background:'#f9f9f9',
-    marginTop:12,
-    border:'1px solid #ddd'
-  }
-}
+
 export default Zone;
